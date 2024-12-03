3827922b458d55e446e10a6b751584a4dc43af96
deablofk <deablofk@gmail.com>
2024-12-03 02:20:51 -0400

    day 3 (part 2)

--- src/main/java/dev/cwby/aoc2024/day3/Day3.java
+++ src/main/java/dev/cwby/aoc2024/day3/Day3.java
@@ -7,25 +7,9 @@ public class Day3 {
 
   public static void main(String[] args) {
     System.out.println("mulIntructionResult: " + runPart1());
+    System.out.println("multIntructionResultDoAndDontDo: " + runPart2());
   }
 
-  ////  fuck regex
-  //  public static String runPart1() {
-  //    Pattern pattern = Pattern.compile("mul\\((-?\\d+),(-?\\d+)\\)");
-  //    Matcher matcher = pattern.matcher(content);
-  //
-  //    int totalAmount = 0;
-  //    while (matcher.find()) {
-  //      String[] values = matcher.group().replace("mul(", "").replace(")", "").split(",");
-  //      int x = Integer.parseInt(values[0].trim());
-  //      int y = Integer.parseInt(values[1].trim());
-  //      totalAmount += (x * y);
-  //    }
-  //
-  //    return String.valueOf(totalAmount);
-  //  }
-
-  // yeahhh baby fuck regex
   public static int runPart1() {
     int result = 0;
     int indexPos = 0;
@@ -62,6 +46,66 @@ public class Day3 {
         result += x * y;
 
         indexPos++;
+      } else {
+        indexPos++;
+      }
+    }
+
+    return result;
+  }
+
+  // look at this porra i'm gonna shut myself out
+  public static int runPart2() {
+    int result = 0;
+    int indexPos = 0;
+    boolean mulEnabled = true;
+
+    while (indexPos < CONTENT.length()) {
+      if (indexPos + 4 <= CONTENT.length() && CONTENT.startsWith("mul(", indexPos)) {
+        if (mulEnabled) {
+          indexPos += 4;
+          int start = indexPos;
+
+          while (indexPos < CONTENT.length()
+              && CONTENT.charAt(indexPos) != ','
+              && Character.isDigit(CONTENT.charAt(indexPos))) {
+            indexPos++;
+          }
+          if (indexPos == start
+              || indexPos >= CONTENT.length()
+              || CONTENT.charAt(indexPos) != ',') {
+            indexPos = start;
+            continue;
+          }
+          int x = Integer.parseInt(CONTENT.substring(start, indexPos).trim());
+          indexPos++;
+
+          start = indexPos;
+
+          while (indexPos < CONTENT.length()
+              && CONTENT.charAt(indexPos) != ')'
+              && Character.isDigit(CONTENT.charAt(indexPos))) {
+            indexPos++;
+          }
+          if (indexPos == start
+              || indexPos >= CONTENT.length()
+              || CONTENT.charAt(indexPos) != ')') {
+            indexPos = start;
+            continue;
+          }
+          int y = Integer.parseInt(CONTENT.substring(start, indexPos).trim());
+          result += x * y;
+
+          indexPos++;
+        } else {
+          indexPos += 4;
+        }
+      } else if (CONTENT.startsWith("do()", indexPos)) {
+        mulEnabled = true;
+        indexPos += 4;
+      } else if (CONTENT.startsWith("don't()", indexPos)) {
+        mulEnabled = false;
+        indexPos += 7;
       } else {
         indexPos++;
       }
